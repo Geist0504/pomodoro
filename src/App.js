e9b4a7bbc8dfb962ad9c72020d63171fb8cd00e9
Andrew Gallin <andrewgallin@gmail.com>
2018-09-23 14:57:30 -0400

    established flex layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+import { faExpandArrowsAlt } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faArrowDown)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <h1>Pomodoro Clock</h1>
+        <div id='interface'>
+          <div id='break'>
+            <h2 id='break-label'> hey to oyou!</h2>
+            <div className='controls'>
+              <FontAwesomeIcon icon={faArrowDown} /><p>50</p><FontAwesomeIcon icon={faArrowDown} />
+            </div>
+          </div>
+          <div id='session'>
+            <h2 id='session-label'> hey to oyou!</h2>
+            <p> 5</p>
+          </div>
+        </div>
+        <h3> Footage vehicle industrial grade man narrative paranoid shrine sign
+        Shibuya ablative long-chain hydrocarbons sunglasses Chiba decay. Tower shrine drugs
+        refrigerator courier rebar into corrupted sign dome Kowloon cyber-boat San Francisco.
+        Bomb denim tattoo corporation pre-range-rover sunglasses car nano-shrine
+        youtube shanty town paranoid. Test</h3>
       </div>
     );
   }
